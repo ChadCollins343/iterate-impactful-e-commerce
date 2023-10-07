bb8060ee901ea412b45adfe1de69a572ad9b112b
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-07 15:57:33 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,27 @@
+import random
+    return data
+
+    main()
+
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+def main():
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    for item in data:    main()
+    for item in data:
+    data = generate_random_data()
+
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    return data
+if __name__ == "__main__":
+import random
+        print(f"Random Number: {item}")
+def main():

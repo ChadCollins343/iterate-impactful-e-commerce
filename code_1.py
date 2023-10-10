a7a4ae5f6675963c202660fd0972e75dd56f4905
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-11 01:03:48 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,30 +1,29 @@
-    const data = generateRandomData();
+        console.log("Random Number: " + item);
     return data;
 }
     for (let i = 0; i < 10; i++) {
-    }
     data.forEach(item => {
-main();
 }
-        console.log("Random Number: " + item);
-
+    }
         data.push(Math.floor(Math.random() * 100) + 1);
-    });
 
+main();
+function main() {
+
+    const data = [];
+    const data = generateRandomData();
 function generateRandomData() {
 
-function main() {
-    const data = [];def main():
+    });    main()
 import random
+    data = generate_random_data()
+if __name__ == "__main__":
+def main():
+
+
+def generate_random_data():
     for item in data:
 
+    data = [random.randint(1, 100) for _ in range(10)]
         print(f"Random Number: {item}")
     return data
-
-if __name__ == "__main__":
-    data = generate_random_data()
-def generate_random_data():
-
-    main()
-
-    data = [random.randint(1, 100) for _ in range(10)]

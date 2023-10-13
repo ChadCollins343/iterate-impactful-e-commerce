b6a58dbc6726a128ff710bfd555787e3a1402942
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-13 07:07:10 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,63 +1,29 @@
+main();
+        console.log("Random Number: " + item);
+    });
+}
+function main() {
+
+    }
+    const data = generateRandomData();
+function generateRandomData() {
+    const data = [];
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+
+}
+    data.forEach(item => {
+    return data;        }
 public class RandomDataGenerator {
         int[] data = new int[10];
-
-        }
-}
-    }
-        }
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
         for (int item : data) {
 
         for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {    for item in data:
-def main():
-    data = generate_random_data()
-    return data
-def generate_random_data():
-    main()
-        print(f"Random Number: {item}")
-import random
-
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-        for (int i = 0; i < 10; i++) {
         }
+
+            System.out.println("Random Number: " + item);
+    }
+}
     public static void main(String[] args) {
-public class RandomDataGenerator {
             data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-        for (int item : data) {
-}
-    }
-
-        }
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-}
-        }
-    }
-
-        int[] data = new int[10];
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-
-        for (int item : data) {
-        }
-public class RandomDataGenerator {
-            data[i] = (int) (Math.random() * 100) + 1;
-    for item in data:
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-    return data
-def generate_random_data():
-def main():
-
-    data = generate_random_data()

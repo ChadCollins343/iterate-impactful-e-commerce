62ae13f02bbe54859459d7f49f44a891d47a8232
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-13 16:07:42 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,29 +1,30 @@
-main();
-        console.log("Random Number: " + item);
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+import random
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+if __name__ == "__main__":
+    main()
+
+
+    for item in data:
+    return data    data.forEach(item => {
     });
+    const data = generateRandomData();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    }
+function generateRandomData() {
 }
 function main() {
 
-    }
-    const data = generateRandomData();
-function generateRandomData() {
-    const data = [];
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-
-}
-    data.forEach(item => {
-    return data;        }
-public class RandomDataGenerator {
-        int[] data = new int[10];
-        for (int item : data) {
-
-        for (int i = 0; i < 10; i++) {
-        }
-
-            System.out.println("Random Number: " + item);
-    }
-}
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
+main();
+    return data;

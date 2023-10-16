4cdc64fe377dcb4c73025c2da4f19164f963a132
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-16 16:13:14 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,29 +1,68 @@
-        console.log("Random Number: " + item);
-    return data;
-}
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-}
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-main();
-function main() {
-
-    const data = [];
-    const data = generateRandomData();
-function generateRandomData() {
-
-    });    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    main()
 import random
-    data = generate_random_data()
+    return data
+
+
 if __name__ == "__main__":
+
+    for item in data:
 def main():
 
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    main()
 
 def generate_random_data():
     for item in data:
+    return data
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+def main():
+import random
+    }
+
+    public static void main(String[] args) {
+}
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+        }
+        }
+public class RandomDataGenerator {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        for (int item : data) {function generateRandomData() {
+    }
+        console.log("Random Number: " + item);
+
+    const data = [];
+function main() {
+}
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+main();
+    return data;
+    const data = generateRandomData();
+    });
+    data = generate_random_data()
+    main()
+
+def main():
+if __name__ == "__main__":
+
 
     data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
+import random
+    for item in data:
+def generate_random_data():
     return data
+        print(f"Random Number: {item}")
